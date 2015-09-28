f6e6d7e1e77c4780a61fa3515a13e4acaa2b43cb
Nikita Hristinin <nkhristinin@gmail.com>
2015-09-28 22:22:41 +1000

    Local font

--- src/app/services/textToSvgService.js
+++ src/app/services/textToSvgService.js
@@ -1,34 +1,44 @@
 angular.module('i8Editor')
-	.factory('textToSvgService', ['$q', 'opentypeService', textToSvgService]);
+    .factory('textToSvgService', ['$q', 'opentypeService', textToSvgService]);
 
 
 function textToSvgService($q, opentypeService) {
-	var pathToFonts = '/src/fonts/'
-	var svgElementBegin = '<svg xmlns="http://www.w3.org/2000/svg">'
-	var svgElementEnd = '</svg>'
+    var pathToFonts = '/src/fonts/'
+    var svgElementBegin = '<svg xmlns="http://www.w3.org/2000/svg">'
+    var svgElementEnd = '</svg>';
+
+    var fonts = {};
 
 
-	var service = {
-		getSVG: getSVG
-	};
+    var service = {
+        getSVG: getSVG
+    };
 
-	return service;
+    return service;
 
-	function getSVG(text, fontName, size) {
-		var deferred = $q.defer();
+    function getSVG(text, fontName, size) {
+        var deferred = $q.defer();
+        var localFont = fonts[fontName];
+        if (localFont) {
+            toSvg(localFont);
+        } else {
+            opentypeService.load(pathToFonts + fontName, function (err, font) {
+                if (!err) {
+                    fonts[fontName] = font;
+                    toSvg(font);
+                } else {
+                    deferred.reject('Error load font');
+                }
+            });
+        }
 
-		opentypeService.load(pathToFonts + fontName, function(err, font) {
-			if (!err) {
-				var path = font.getPath(text, 0, size, size);
-				var pathSvg = path.toSVG();
-				deferred.resolve(svgElementBegin + pathSvg + svgElementEnd);
-			} else {
-				deferred.reject('Error load font');
-			}
 
+        function toSvg(font) {
+            var path = font.getPath(text, 0, size, size);
+            var pathSvg = path.toSVG();
+            deferred.resolve(svgElementBegin + pathSvg + svgElementEnd);
+        }
 
-		});
-
-		return deferred.promise;
-	}
+        return deferred.promise;
+    }
 }
